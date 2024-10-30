cf476dc44c4f6fe97a4e687b17928ccb96ac1639
HTL2910 <hathanhlong2910@gmail.com>
2024-10-30 12:19:25 +0700

    update get product rating,title and write data to csv

--- Scraper/Amazon.py
+++ Scraper/Amazon.py
@@ -17,20 +17,49 @@ def get_product_price(soup):
         'class':'a-price-whole'
     })
     
-    return main_price_span.get_text(strip=True).replace('.','')
+    return main_price_span.get_text(strip=True).strip().replace('.','')
+def get_product_title(soup):
+    product_title=soup.find('span',id='productTitle')
+    return product_title.text.strip()
+
+def get_product_rating(soup):
+    product_rating_div=soup.find('div',attrs={
+        'id':'averageCustomerReviews'
+    })
+    product_rating_section=product_rating_div.find('span', id='acrPopover',attrs={
+        'class':"reviewCountTextLinkedHistogram noUnderline"
+    })
+
+    product_rating=product_rating_section.find('span',attrs={
+        'class':'a-icon-alt'
+    })
+    return float(product_rating.get_text(strip=True).strip().split()[0])
+
 
 def extract_product_info(url):
     product_info={}
     html=get_page_html(url)
     soup=bs4.BeautifulSoup(html,'lxml')
 
+    product_info['title']=get_product_title(soup)
     product_info['price']=get_product_price(soup)
+    product_info['rating']=get_product_rating(soup)
+    print(product_info)
     return product_info
 
 
 if __name__=="__main__":
+    product_data=[]
     with open("amazon_web.csv",newline='')as csvFile:
         reader=csv.reader(csvFile,delimiter=',')
         for row in reader:
             url=row[0]
-            print(extract_product_info(url))
+            product_data.append(extract_product_info(url))
+    output_file='amazon_product_data-{}.csv'.format(
+        datetime.today().strftime("%d_%m_%Y")
+    )
+    with open(output_file,'w',newline='')as outputFile:
+        write=csv.writer(outputFile)
+        write.writerow(product_data[0].keys())
+        for product in product_data:
+            write.writerow(product.values())
