5a514d6c3b3ffa32f526bef7019361bdeba83d58
HTL2910 <hathanhlong2910@gmail.com>
2024-10-31 23:46:46 +0700

    selenium test bot navigation to link

--- Scraper/Selenium/Bot_test.py
+++ Scraper/Selenium/Bot_test.py
@@ -2,11 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
 import time 
 from datetime import date
 import os
 import json
 
+service = Service()
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=options)
+def main():
+    try:
+        driver.get('https://trello.com')
+        input('Bot Operation Completed.Press any key...')
+        driver.close()
+    except Exception as e:
+        print(e)
 if __name__=="__main__":
-    print(1)
+    main()
     
