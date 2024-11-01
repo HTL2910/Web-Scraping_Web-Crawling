ced3b8523f759eb3f43151f5b5ac261f53701506
HTL2910 <hathanhlong2910@gmail.com>
2024-11-01 12:18:26 +0700

    add click button and login with google

--- Scraper/Selenium/Bot_test.py
+++ Scraper/Selenium/Bot_test.py
@@ -10,14 +10,42 @@ import json
 
 service = Service()
 options = webdriver.ChromeOptions()
+
+options.add_argument('--disable-gpu')  # Vô hiệu hóa GPU
+options.add_argument('--no-sandbox')  # Tắt sandbox
+options.add_argument('--disable-dev-shm-usage')  # Giảm sử dụng bộ nhớ chia sẻ
+options.add_argument('--disable-software-rasterizer') 
 driver = webdriver.Chrome(service=service, options=options)
+
+def login():
+    with open('D:/Python_Project/Web-Scraping_Web-Crawling/Scraper/Selenium/config.json') as configFile:
+        credentials = json.load(configFile)
+    time.sleep(2)
+    driver.find_element(By.XPATH, value="//a[contains(@href, '/login')]").click()    
+    time.sleep(2)
+    #nhập user name
+    # username = driver.find_element(By.CSS_SELECTOR, value="input[name='username']")
+    # username.clear()
+    # username.send_keys(credentials["USERNAME"])
+    # time.sleep(2)
+
+
+    #login with google
+    driver.find_element(By.ID,value='google-auth-button').click()
+    # Click nút submit
+    # driver.find_element(By.ID, value="login-submit").click()
+    time.sleep(2)
 def main():
     try:
+        # Truy cập trang chủ Trello
         driver.get('https://trello.com')
-        input('Bot Operation Completed.Press any key...')
+        login()
+        input('Bot Operation Completed. Press any key...')
         driver.close()
     except Exception as e:
         print(e)
+    finally:
+        driver.close()
 if __name__=="__main__":
     main()
     
