ab4d8764b063e6b0741ac26ede5a585c46165a22
HTL2910 <hathanhlong2910@gmail.com>
2024-11-08 18:22:42 +0700

    move action

--- Scraper/Selenium/Link_Navigation.py
+++ Scraper/Selenium/Link_Navigation.py
@@ -12,6 +12,7 @@ NO_THREADS=5
 url='https://www.python.org/'
 
 def find_element_driver(driver):
+    driver = webdriver.Chrome(service=service, options=options)
     ele=driver.find_element(By.ID,'mainnav')#by ID 
     ele_1=driver.find_element(By.CLASS_NAME,'donate-button')#By class name
     ele_2=driver.find_element(By.XPATH,'//*[@id="touchnav-wrapper"]/header/div/div[1]/a')#By xpath
@@ -22,29 +23,39 @@ def find_element_driver(driver):
  
     print(ele_6)
 
-def action_link(driver,action):
-    button=driver.find_element(By.CLASS_NAME,'donate-button')
-    action.click(on_element=button)
-    action.double_click(on_element=button)
-    action.click_and_hold(on_element=button)
-    action.release(button)
+def action_link(driver):
+    # button=driver.find_element(By.CLASS_NAME,'donate-button')
+    btn=driver.find_element(By.LINK_TEXT,'Downloads')
+
+    action=ActionChains(driver)
+    # action.click(on_element=button)
+    # action.double_click(on_element=button)
+    # action.click_and_hold(on_element=button)
+    # action.release(button)
+    # action.move_by_offset(100,10)
+    # action.move_to_element(to_element=button)
+    action.move_to_element_with_offset(btn,150,0).click()
+
     action.perform()
 
 def test_navigation_link(url):
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get(url)
     # find_element_driver(driver)
-    action_link(driver,action)
+    action_link(driver)
     
-    driver.close()
-    # driver.quit()
     time.sleep(12)
+    driver.close()
+    driver.quit()
+   
 
 
 if __name__=="__main__":
     service = Service()
     options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=options)
-    action=ActionChains(driver)
+    
+    
+    
     ###########ThreadPoolExecutor
     #  with concurrent.futures.ThreadPoolExecutor(max_workers=NO_THREADS) as executor:
     #     for wkn in tqdm(range(10)):
