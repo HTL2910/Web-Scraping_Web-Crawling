0263a1ce6695e616815bfcac2fd9bc05b9a96678
HTL2910 <hathanhlong2910@gmail.com>
2024-11-08 17:34:45 +0700

    action button with actionChains

--- Scraper/Selenium/Link_Navigation.py
+++ Scraper/Selenium/Link_Navigation.py
@@ -5,9 +5,10 @@ import time
 import concurrent.futures
 from tqdm import tqdm
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 NO_THREADS=5
+
 url='https://www.python.org/'
 
 def find_element_driver(driver):
@@ -21,19 +22,32 @@ def find_element_driver(driver):
  
     print(ele_6)
 
+def action_link(driver,action):
+    button=driver.find_element(By.CLASS_NAME,'donate-button')
+    action.click(on_element=button)
+    action.double_click(on_element=button)
+    action.click_and_hold(on_element=button)
+    action.release(button)
+    action.perform()
+
 def test_navigation_link(url):
-    
-    service = Service()
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=options)
     driver.get(url)
-    find_element_driver(driver)
-    driver.quit()
+    # find_element_driver(driver)
+    action_link(driver,action)
+    
+    driver.close()
+    # driver.quit()
     time.sleep(12)
 
 
 if __name__=="__main__":
+    service = Service()
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+    action=ActionChains(driver)
+    ###########ThreadPoolExecutor
     #  with concurrent.futures.ThreadPoolExecutor(max_workers=NO_THREADS) as executor:
     #     for wkn in tqdm(range(10)):
     #         executor.submit(test_navigation_link,'https://hathanhlong2910.itch.io/roll-ball-3d')
+    ###########ThreadPoolExecutor
     test_navigation_link(url)
